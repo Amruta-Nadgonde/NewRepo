0ca26082e7064f63c76a4b08f683d1b8f06a03a1
amruta-nadgonde <amruta.nadgonde@gmail.com>
2022-07-04 15:39:36 +0530

    Chnages made by tester

--- DemoJava/src/test/CalculatorTest.java
+++ DemoJava/src/test/CalculatorTest.java
@@ -13,4 +13,14 @@ public class CalculatorTest {
 	  int actres = Calculator.add(a, b);
 	  assertEquals(actres, expres);
   }
+  
+  @Test
+  public void mulTest() {
+	  Calculator cal = new Calculator();
+	  int a = 7;
+	  int b=10;
+	  int expres = 70;
+	  int actres = cal.multiply(a, b);
+	  assertEquals(actres, expres);
+  }
 }
